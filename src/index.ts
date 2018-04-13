5c3f2c5a7469fa2d0cc39f09ea1ad7e5ae58e144
Nelson Chen <crazysim@gmail.com>
2018-04-13 00:03:00 -0700

    Hello World

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,32 @@ import { Request, Response } from "express";
 import GoogleAssistant from './assistant';
 import * as path from 'path';
 
+const startConversation = (conversation) => {
+  // setup the conversation
+  conversation
+    .on('response', text => console.log('Assistant Response:', text))
+    // if we've requested a volume level change, get the percentage of the new level
+    .on('volume-percent', percent => console.log('New Volume Percent:', percent))
+    // the device needs to complete an action
+    .on('device-action', action => console.log('Device Action:', action))
+    // once the conversation is ended, see if we need to follow up
+    .on('ended', (error, continueConversation) => {
+      if (error) {
+        console.log('Conversation Ended Error:', error);
+      } else if (continueConversation) {
+        console.log('Conversation Continued? Bye anyway.', error);
+        conversation.end();
+      } else {
+        console.log('Conversation Complete');
+        conversation.end();
+      }
+    })
+    // catch any errors
+    .on('error', (error) => {
+      console.log('Conversation Error:', error);
+    });
+};
+
 export function broadcastMessage(req: Request, res: Response) {
   const config = {
     auth: {
@@ -9,13 +35,22 @@ export function broadcastMessage(req: Request, res: Response) {
       savedTokensBucket: 'broadcast-assistant-37619.appspot.com',
     }
   };
-  console.log(req)
 
   const assistant = new GoogleAssistant(config.auth);
+  assistant
+    .on('ready', () => {
+      assistant.start(
+        {
+          lang: 'en-US', // defaults to en-US, but try other ones, it's fun!
+          textQuery: 'broadcast Hello World!'
+        },
+        startConversation
+      )
+    })
 
   res
-      .status(200)
-      .type("application/json")
-      .send("{ \"result\": \"Hello World!\"}")
-      .end()
+    .status(200)
+    .type("application/json")
+    .send("{ \"result\": \"Hello World!\"}")
+    .end()
 }
